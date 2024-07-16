630c1ca83b30507d217b0e45d141c5b477b63d6d
ET-SPARK <samuelwoyesso2016@gmail.com>
2024-07-16 21:04:44 +0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Image, StyleSheet, Platform, View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
